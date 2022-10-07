b5217bda701b76d774c73cc74737e410de0439b3
Dinh Quoc Han <dqh@dinhquochan.com>
2022-10-07 09:52:47 +0700

    First

---
+++ src/LaravelHeroiconsServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ArchiElite\LaravelHeroicons;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelHeroiconsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        //
+    }
+}
